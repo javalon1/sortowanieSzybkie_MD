8c95703d8817b040a2226d776d70872801caae7d
Maciej Drobot <maciejdrobot@gmail.com>
2019-05-21 22:03:15 +0100

    QuickSort dziala - sortuje :)

--- src/main/java/com/sda/algorytmy/Test.java
+++ src/main/java/com/sda/algorytmy/Test.java
@@ -13,25 +13,9 @@ public class Test {
         int pivot = 3;
         int[]merge = new int[tablica.length + tablica2.length + 1];
 
-        //merge = Arrays.copyOfRange(tablica, 2, tablica.length - 1);
-        //merge = Arrays.copyOfRange(tablica2, 2, tablica.length - 1);
-        //merge = Arrays.copyOfRange(pivot);
-
-//        System.arraycopy(tablica, 0, merge, 0, tablica.length);
-//        merge[tablica.length] = pivot;
-//        System.arraycopy(tablica2, 0, merge, tablica.length + 1, tablica2.length);
-
-//        System.out.println(Arrays.toString(merge));
-//        System.out.println(merge.length);
 
         int ileWiekszych = 4;
         int[] wieksze = new int[ileWiekszych];
-
-//        for (int i = 0; i < ileWiekszych; i++){
-//            if (tablica[i] > pivot){
-//                wieksze[i] = tablica[i];
-//            }
-//        }
 
 
     }
